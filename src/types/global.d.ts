d2c75123d41275d1968df95659a9e9b5b6545c08
Nathan Silva Santos <nathan@w16.com.br>
2022-05-25 00:07:11 -0300

    prettier printWidth

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+type FetchStatus = 'idle' | 'fetching' | 'error' | 'success';
+
 type StoreActionResponse = {
   status: number;
   data?: any;
   message?: string;
 };
-
-type FetchStatus = 'idle' | 'fetching' | 'error' | 'success';
 
 type User = {
   id: number;
